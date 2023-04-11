19de7b5bcac86a039a51885378ef02240e2abad1
ravi <ravikumar6f@gmail.com>
2023-04-11 10:46:09 +0530

    registering the post model to the admin pagge to make changes from the admin page itself

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # consider that after a db call we have a list of dictionaries of the post 
-
-posts=[
-    {
-        'author':'Ravi',
-        'content' : 'First post on this site',
-        'date_posted': "august month post",
-        'title': 'post1'
-    },
-    {
+#dummy datasets
+# posts=[
+#     {
+#         'author':'Ravi',
+#         'content' : 'First post on this site',
+#         'date_posted': "august month post",
+#         'title': 'post1'
+#     },
+#     {
         
-        'author':'modi',
-        'content' : 'Second post on this site',
-        'date_posted': "august month post",
-        'title':'post2'
-    }
-]
+#         'author':'modi',
+#         'content' : 'Second post on this site',
+#         'date_posted': "august month post",
+#         'title':'post2'
+#     }
+# ]
 
 
 def home(request):
     context={
-        'posts':posts,
+        'posts':Post.objects.all(),
         'title': "Home"
     }
     return render(request, 'blog/blog_home.html', context)
